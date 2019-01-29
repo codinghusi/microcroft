71a0588a660fcebdad73b5bf2fa72d1063f0eda4
Noah Kempers <nkempers@outlook.de>
2019-01-29 11:16:00 +0100

    new setup for VSCode and better libary support (esp32 wifiudp.h,.....)

---
+++ newclient/microcroft/src/main.cpp
@@ -0,0 +1,29 @@
+
+#include "../include/lib.h"
+
+// custom config
+const char* networkSSID = "ITA";
+const char* networkPassword = "Pruefung2018";
+
+
+
+// THE following is magic :D ... dON'T tOUCH iT!
+
+// expert config
+const int port = 6522;
+const int broadcastPort = 6523;
+
+void setup() {
+  Serial.begin( 115200 );
+  Serial.println( "Connecting to Wifi ..." );
+  connectToWifi( networkSSID, networkPassword );
+  Serial.println( "Connecting to Picroft ..." );
+  connectToServer( broadcastPort, port );
+  Serial.println( "Init Communication ..." );
+  initCommunication();
+  Serial.println( "Running!" );
+}
+
+void loop() {
+  handleCommunication();
+}
